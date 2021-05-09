3ef2bea4c6644e1d927886a775c958018abfad6d
Naeimah Alsaleh <>
2021-05-09 01:26:33 +0000

    part 1

--- pollapp/admin.py
+++ pollapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from.models import poll 
+from.models import option
+from.models import response
+
+class polladmin (admin.ModelAdmin):
+  list_display = ("question", "active_until", "status",)
+  list_filter = ("status",)
+  search_fields = ("question",)
+
+admin.site.register(poll)
+admin.site.register(option)
+admin.site.register(response)
+
+
+
+
+
--- pollapp/models.py
+++ pollapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+  )
+
+
+class poll(models.Model):
+
+  title = models.CharField(max_length=200, unique=True)
+  question = models.CharField(max_length=200, unique=True)
+  active_until = models.DateTimeField(auto_now_add=True)
+  id = models.BigAutoField(primary_key=True)
+  status = models.IntegerField(choices=STATUS,default=0)
+
+class options (models.Model):
+  title = models.CharField(max_length=200, unique=True)
+  poll = models.ForeignKey("poll", on_delete=models.CASCADE)
+
+  
+class response(models.Model):
+  name = models.CharField(max_length=200, unique=True)
+  response_time = models.DatetimeField(auto_now_add= True)
+  options = models.ForeingKey("options", default =None , on_delete = models.CASECADE)
+
